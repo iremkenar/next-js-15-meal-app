7cb9fa3188c5c8652cde6e3afd011552739fdc05
IRKENAR <irem.kenar@outlook.com>
2026-01-18 21:20:35 +0300

    Add projects assets

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <p>Hello World!</p>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
     </main>
   );
 }
