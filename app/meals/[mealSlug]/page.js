f6840fcb707b0bc617ee7924569e642774b1da14
IRKENAR <irem.kenar@outlook.com>
2026-01-27 17:34:49 +0300

    Add dynamic metadata

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -4,6 +4,19 @@ import { notFound } from 'next/navigation';
 import { getMeal } from '@/lib/meals';
 import classes from './page.module.css';
 
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 export default async function MealDetailsPage({ params }) {
   const paramValues = await params;
   const meal = getMeal(paramValues.mealSlug);
